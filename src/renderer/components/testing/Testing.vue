547f48e6d233888070bebb36ca482ddfafe7b739
Fabrice Bellingard <bellingard@gmail.com>
2017-08-23 23:24:41 +0200

    Move examples to testing folder

---
+++ src/renderer/components/testing/Testing.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div>
+    <router-link to="/initial-landing-page">Initial Landing Page</router-link>
+    <br/>
+    <router-link to="/vuetify-tests">Vuetify Tests</router-link>
+  </div>
+
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+</style>
