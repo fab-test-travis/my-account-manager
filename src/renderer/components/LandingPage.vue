529a4a0af66de53f7088f10ba23840078ecb0f08
Fabrice Bellingard <bellingard@gmail.com>
2017-08-31 21:33:02 +0200

    Adjust formatting settings

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,8 +1,7 @@
 <template>
-
   <v-app dark>
 
-     <v-navigation-drawer
+    <v-navigation-drawer
       permanent
       :mini-variant.sync="mini"
     >
@@ -31,7 +30,10 @@
 
       <v-subheader class="mt-2 grey--text text--darken-1">MANAGE</v-subheader>
       <v-list dense>
-        <v-list-tile v-for="item in manageItems" :key="item.text">
+        <v-list-tile
+          v-for="item in manageItems"
+          :key="item.text"
+        >
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -40,7 +42,7 @@
               {{ item.text }}
             </v-list-tile-title>
           </v-list-tile-content>
-        </v-list-tile>
+          </v-list-tile>
       </v-list>
 
       <v-subheader class="mt-2 grey--text text--darken-1">ADMIN</v-subheader>
@@ -57,114 +59,120 @@
         </v-list-tile>
       </v-list>
 
-    </v-navigation-drawer>
-    
-    <v-toolbar fixed class="blue darken-1">
-      <v-toolbar-title>
-        <v-toolbar-side-icon @click.stop="mini = !mini"></v-toolbar-side-icon>
-        <v-btn
-          class="grey darken-3"
-          :loading="loading"
-          @click.native="loader = 'loading'"
-          :disabled="loading"
-        >
-          Load
-          <v-icon right>file_upload</v-icon>
-        </v-btn>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-icon>help</v-icon>
-    </v-toolbar>
+      </v-navigation-drawer>
 
-    <main>
-      <v-container fluid>
-          <v-select
-            :disabled="repository == null"
-            :items="accounts"
-            v-model="selectedAccount"
-            label="Select account"
-            item-text="name"
-            item-value="id"
-          ></v-select>
-          <v-list v-if="selectedAccount" two-line>
-            <template v-for="transaction in transactions">
-              <v-list-tile :key="transaction.id">
-                <v-list-tile-content>
-                  <v-list-tile-title v-html="transaction.amount"></v-list-tile-title>
-                  <v-list-tile-sub-title v-html="transaction.date"></v-list-tile-sub-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </template>
-          </v-list>
-      </v-container>
-    </main>
+      <v-toolbar
+        fixed
+        class="blue darken-1"
+      >
+        <v-toolbar-title>
+          <v-toolbar-side-icon @click.stop="mini = !mini"></v-toolbar-side-icon>
+          <v-btn
+            class="grey darken-3"
+            :loading="loading"
+            @click.native="loader = 'loading'"
+            :disabled="loading"
+          >
+            Load
+            <v-icon right>file_upload</v-icon>
+            </v-btn>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-icon>help</v-icon>
+        </v-toolbar>
 
-    <v-footer></v-footer>
+        <main>
+          <v-container fluid>
+            <v-select
+              :disabled="repository == null"
+              :items="accounts"
+              v-model="selectedAccount"
+              label="Select account"
+              item-text="name"
+              item-value="id"
+            ></v-select>
+              <v-list
+                v-if="selectedAccount"
+                two-line
+              >
+                <template v-for="transaction in transactions">
+                  <v-list-tile :key="transaction.id">
+                    <v-list-tile-content>
+                      <v-list-tile-title v-html="transaction.amount"></v-list-tile-title>
+                      <v-list-tile-sub-title v-html="transaction.date"></v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </template>
+                </v-list>
+          </v-container>
+        </main>
+
+        <v-footer></v-footer>
 
   </v-app>
-
 </template>
 
 <script>
-  import * as _ from 'lodash'
-  import StorageService from '../services/StorageService'
+import * as _ from 'lodash'
+import StorageService from '../services/StorageService'
 
-  const storageService = new StorageService()
+const storageService = new StorageService()
 
-  export default {
-    name: 'landing-page',
-    data () {
-      return {
-        mini: false,
-        loader: null,
-        loading: false,
-        repository: null,
-        selectedAccount: null,
-        manageItems: [
-          { icon: 'attach_money', text: 'Accounts' },
-          { icon: 'group_work', text: 'Categories' },
-          { icon: 'account_balance', text: 'Institutions' },
-          { icon: 'people', text: 'Payees' }
-        ]
-      }
+export default {
+  name: 'landing-page',
+  data () {
+    return {
+      mini: false,
+      loader: null,
+      loading: false,
+      repository: null,
+      selectedAccount: null,
+      manageItems: [
+        { icon: 'attach_money', text: 'Accounts' },
+        { icon: 'group_work', text: 'Categories' },
+        { icon: 'account_balance', text: 'Institutions' },
+        { icon: 'people', text: 'Payees' }
+      ]
+    }
+  },
+  computed: {
+    accounts () {
+      return this.repository ? _.values(this.repository.bankAccounts) : []
     },
-    computed: {
-      accounts () {
-        return this.repository ? _.values(this.repository.bankAccounts) : []
-      },
-      transactions () {
-        if (this.selectedAccount == null) {
-          return []
+    transactions () {
+      if (this.selectedAccount == null) {
+        return []
+      }
+      let tList = []
+      // couldn't manage to use lodash here... :-(
+      _.values(this.repository.transactions).forEach(t => {
+        if (this.selectedAccount === t.fromId || this.selectedAccount === t.toId) {
+          tList.push(t)
         }
-        let tList = []
-        // couldn't manage to use lodash here... :-(
-        _.values(this.repository.transactions).forEach(t => {
-          if (this.selectedAccount === t.fromId || this.selectedAccount === t.toId) {
-            tList.push(t)
-          }
-        })
-        return tList
-      }
-    },
-    watch: {
-      loader () {
-        const l = this.loader
-        this[l] = !this[l]
+      })
+      return tList
+    }
+  },
+  watch: {
+    loader () {
+      const l = this.loader
+      this[l] = !this[l]
 
-        storageService.loadFromJson('', (err, obj) => {
-          if (err) {
-            console.error(err)
-          } else {
-            this.repository = storageService.repo
-          }
-          this[l] = false
-          this.loader = null
-        })
-      }
+      storageService.loadFromJson('', (err, obj) => {
+        if (err) {
+          console.error(err)
+        } else {
+          this.repository = storageService.repo
+        }
+        this[l] = false
+        this.loader = null
+      })
     }
   }
+}
 
 </script>
 
 <style>
+
 </style>
