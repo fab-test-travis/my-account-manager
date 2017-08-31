2968785a1b9959ee7cd06be1a43e375a8b57777c
Fabrice Bellingard <bellingard@gmail.com>
2017-08-31 21:51:45 +0200

    Extract Nav Bar

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,138 +1,75 @@
 <template>
   <v-app dark>
 
-    <v-navigation-drawer
-      permanent
-      :mini-variant.sync="mini"
+    <nav-bar></nav-bar>
+
+    <v-toolbar
+      fixed
+      class="blue darken-1"
     >
-      <div class="pa-3 mt-3 mb-3">
-        <v-toolbar-title>
-          My Account Manager
-          <v-icon>monetization_on</v-icon>
-        </v-toolbar-title>
-        <div class="text-xs-center">Version x.x</div>
-      </div>
-
-      <v-divider></v-divider>
-
-      <v-list dense>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>
-              Dashboards
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-
-      <v-subheader class="mt-2 grey--text text--darken-1">MANAGE</v-subheader>
-      <v-list dense>
-        <v-list-tile
-          v-for="item in manageItems"
-          :key="item.text"
+      <v-toolbar-title>
+        <v-btn
+          class="grey darken-3"
+          :loading="loading"
+          @click.native="loader = 'loading'"
+          :disabled="loading"
         >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>
-              {{ item.text }}
-            </v-list-tile-title>
-          </v-list-tile-content>
-          </v-list-tile>
-      </v-list>
+          Load
+          <v-icon right>file_upload</v-icon>
+          </v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-icon>help</v-icon>
 
-      <v-subheader class="mt-2 grey--text text--darken-1">ADMIN</v-subheader>
-      <v-list dense>
-        <v-list-tile>
-          <v-list-tile-action>
-            <v-icon>settings</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>
-              Settings
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+      </v-toolbar>
 
-      </v-navigation-drawer>
+      <main>
+        <v-container fluid>
+          <v-select
+            :disabled="repository == null"
+            :items="accounts"
+            v-model="selectedAccount"
+            label="Select account"
+            item-text="name"
+            item-value="id"
+          ></v-select>
+            <v-list
+              v-if="selectedAccount"
+              two-line
+            >
+              <template v-for="transaction in transactions">
+                <v-list-tile :key="transaction.id">
+                  <v-list-tile-content>
+                    <v-list-tile-title v-html="transaction.amount"></v-list-tile-title>
+                    <v-list-tile-sub-title v-html="transaction.date"></v-list-tile-sub-title>
+                  </v-list-tile-content>
+                </v-list-tile>
+              </template>
+              </v-list>
+        </v-container>
+      </main>
 
-      <v-toolbar
-        fixed
-        class="blue darken-1"
-      >
-        <v-toolbar-title>
-          <v-toolbar-side-icon @click.stop="mini = !mini"></v-toolbar-side-icon>
-          <v-btn
-            class="grey darken-3"
-            :loading="loading"
-            @click.native="loader = 'loading'"
-            :disabled="loading"
-          >
-            Load
-            <v-icon right>file_upload</v-icon>
-            </v-btn>
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-icon>help</v-icon>
-        </v-toolbar>
-
-        <main>
-          <v-container fluid>
-            <v-select
-              :disabled="repository == null"
-              :items="accounts"
-              v-model="selectedAccount"
-              label="Select account"
-              item-text="name"
-              item-value="id"
-            ></v-select>
-              <v-list
-                v-if="selectedAccount"
-                two-line
-              >
-                <template v-for="transaction in transactions">
-                  <v-list-tile :key="transaction.id">
-                    <v-list-tile-content>
-                      <v-list-tile-title v-html="transaction.amount"></v-list-tile-title>
-                      <v-list-tile-sub-title v-html="transaction.date"></v-list-tile-sub-title>
-                    </v-list-tile-content>
-                  </v-list-tile>
-                </template>
-                </v-list>
-          </v-container>
-        </main>
-
-        <v-footer></v-footer>
+      <v-footer></v-footer>
 
   </v-app>
 </template>
 
 <script>
 import * as _ from 'lodash'
+import NavBar from './LandingPage/NavBar'
 import StorageService from '../services/StorageService'
 
 const storageService = new StorageService()
 
 export default {
   name: 'landing-page',
+  components: { NavBar },
   data () {
     return {
-      mini: false,
       loader: null,
       loading: false,
       repository: null,
-      selectedAccount: null,
-      manageItems: [
-        { icon: 'attach_money', text: 'Accounts' },
-        { icon: 'group_work', text: 'Categories' },
-        { icon: 'account_balance', text: 'Institutions' },
-        { icon: 'people', text: 'Payees' }
-      ]
+      selectedAccount: null
     }
   },
   computed: {
---
+++ src/renderer/components/LandingPage/NavBar.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-navigation-drawer
+    permanent
+    :mini-variant.sync="mini"
+  >
+
+    <div :class="miniIconStyle">
+      <v-toolbar-side-icon @click.stop="mini = !mini">
+      </v-toolbar-side-icon>
+    </div>
+
+    <div class="pa-3 mt-3 mb-3">
+      <v-toolbar-title>
+        My Account Manager
+        <v-icon>monetization_on</v-icon>
+      </v-toolbar-title>
+      <div class="text-xs-center">Version x.x</div>
+    </div>
+
+    <v-divider></v-divider>
+
+    <v-list dense>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>dashboard</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Dashboards
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+
+    <v-subheader class="mt-2 grey--text text--darken-1">MANAGE</v-subheader>
+    <v-list dense>
+      <v-list-tile
+        v-for="item in manageItems"
+        :key="item.text"
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            {{ item.text }}
+          </v-list-tile-title>
+        </v-list-tile-content>
+        </v-list-tile>
+    </v-list>
+
+    <v-subheader class="mt-2 grey--text text--darken-1">ADMIN</v-subheader>
+    <v-list dense>
+      <v-list-tile>
+        <v-list-tile-action>
+          <v-icon>settings</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title>
+            Settings
+          </v-list-tile-title>
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+
+    </v-navigation-drawer>
+</template>
+
+<script>
+
+export default {
+  name: 'nav-bar',
+  data () {
+    return {
+      mini: false,
+      manageItems: [
+        { icon: 'attach_money', text: 'Accounts' },
+        { icon: 'group_work', text: 'Categories' },
+        { icon: 'account_balance', text: 'Institutions' },
+        { icon: 'people', text: 'Payees' }
+      ]
+    }
+  },
+  computed: {
+    miniIconStyle () {
+      return this.mini ? 'text-xs-center' : ''
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
