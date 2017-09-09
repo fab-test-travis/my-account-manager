a166a19e5529834d6006640fd18e1fee646878ba
Fabrice Bellingard <bellingard@gmail.com>
2017-09-09 17:55:34 +0200

    Display balance on account page

--- src/renderer/components/pages/Accounts.vue
+++ src/renderer/components/pages/Accounts.vue
@@ -3,61 +3,72 @@
     <v-container fluid>
       <v-layout row
                 wrap>
-        <v-flex xs3>
-          <v-select :disabled="!this.$repo.isLoaded()"
-                    :items="accounts"
-                    v-model="selectedAccount"
-                    label="Select account"
-                    item-text="name"
-                    item-value="id">
-          </v-select>
+        <v-flex xs3
+                class="pr-4">
+          <div>
+            <v-select :disabled="!this.$repo.isLoaded()"
+                      :items="accounts"
+                      v-model="selectedAccount"
+                      label="Select account"
+                      item-text="name"
+                      item-value="id">
+            </v-select>
+          </div>
+          <div>
+            <div class="grey--text text--darken-1">
+              Balance
+            </div>
+            <div class="text-xs-right headline">
+              {{ $format.amount(accountBalance) }} &euro;
+            </div>
+          </div>
+          <div class="mt-5">
+            <v-text-field append-icon="search"
+                          label="Search"
+                          single-line
+                          hide-details
+                          v-model="search"
+                          class="">
+            </v-text-field>
+          </div>
         </v-flex>
 
-        <v-flex xs5>
-        </v-flex>
+        <v-flex xs9>
 
-        <v-flex xs4>
-          <v-text-field append-icon="search"
-                        label="Search"
-                        single-line
-                        hide-details
-                        v-model="search"
-                        class="">
-          </v-text-field>
+          <v-data-table :headers="headers"
+                        :items="transactions"
+                        :rows-per-page-items="pagination.size"
+                        :pagination.sync="pagination.sort"
+                        :search="search">
+            <template slot="items"
+                      scope="props">
+              <td class="text-xs-left"
+                  v-tooltip:top="{ html: props.item.id }">
+                <div style="font-weight: bold; font-size: 16px">{{ $format.dateInYear(props.item.date) }}</div>
+                <div>{{ $format.year(props.item.date) }}</div>
+              </td>
+              <td class="text-xs-left">
+                <div v-if="$repo.bankAccount(props.item.toId) != null && $repo.bankAccount(props.item.fromId) != null">
+                  Virement {{ selectedAccount === props.item.fromId ? 'depuis' : 'vers' }} {{ selectedAccount === props.item.fromId ? $format.categoryName(props.item.toId) : $format.categoryName(props.item.fromId) }}
+                </div>
+                <div v-tooltip:left="{ html: props.item.payeeId }">{{ $format.payeeName(props.item.payeeId) }}</div>
+                <div>{{ props.item.desc }}</div>
+              </td>
+              <td v-if="$repo.bankAccount(props.item.toId) != null && $repo.bankAccount(props.item.fromId) != null"
+                  class="text-xs-center">
+                Virement
+              </td>
+              <td v-else
+                  class="text-xs-center">
+                <div v-tooltip:top="{ html: props.item.fromId }">{{ $format.categoryName(props.item.fromId) }}</div>
+              </td>
+              <td class="text-xs-right"
+                  :class="$format.colorForAmount(selectedAccount === props.item.toId ? props.item.amount : -props.item.amount)">
+                {{ $format.amount(selectedAccount === props.item.toId ? props.item.amount : -props.item.amount) }}
+              </td>
+            </template>
+          </v-data-table>
         </v-flex>
-
-        <v-data-table :headers="headers"
-                      :items="transactions"
-                      :rows-per-page-items="pagination.size"
-                      :pagination.sync="pagination.sort"
-                      :search="search">
-          <template slot="items"
-                    scope="props">
-            <td class="text-xs-left"
-                v-tooltip:top="{ html: props.item.id }">
-              <div style="font-weight: bold; font-size: 16px">{{ $format.dateInYear(props.item.date) }}</div>
-              <div>{{ $format.year(props.item.date) }}</div>
-            </td>
-            <td class="text-xs-left">
-              <div v-if="$repo.bankAccount(props.item.toId) != null && $repo.bankAccount(props.item.fromId) != null">
-                Virement {{ selectedAccount === props.item.fromId ? 'depuis' : 'vers' }} 
-                {{ selectedAccount === props.item.fromId ? $format.categoryName(props.item.toId) : $format.categoryName(props.item.fromId) }}
-              </div>
-              <div v-tooltip:left="{ html: props.item.payeeId }">{{ $format.payeeName(props.item.payeeId) }}</div>
-              <div>{{ props.item.desc }}</div>
-            </td>
-            <td v-if="$repo.bankAccount(props.item.toId) != null && $repo.bankAccount(props.item.fromId) != null" class="text-xs-center">
-              Virement
-            </td>
-            <td v-else class="text-xs-center">
-              <div v-tooltip:top="{ html: props.item.fromId }">{{ $format.categoryName(props.item.fromId) }}</div>
-            </td>
-            <td class="text-xs-right"
-                :class="$format.colorForAmount(selectedAccount === props.item.toId ? props.item.amount : -props.item.amount)">
-              {{ $format.amount(selectedAccount === props.item.toId ? props.item.amount : -props.item.amount) }}
-            </td>
-          </template>
-        </v-data-table>
 
       </v-layout>
     </v-container>
@@ -93,6 +104,9 @@ export default {
     },
     transactions() {
       return this.$repo.isLoaded() && this.selectedAccount != null ? this.$repo.transactionsForAccount(this.selectedAccount) : []
+    },
+    accountBalance() {
+      return this.$repo.isLoaded() && this.selectedAccount != null ? this.$repo.getAccountBalance(this.selectedAccount) : ''
     }
   }
 }
